cd9a9a45930cd694d23bce1f771d8c1d46d73945
Zac Willmington <zac.willmington@gmail.com>
2018-04-16 16:04:02 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -8,23 +8,28 @@
 
 def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_name
 "SELECT projects.title, SUM(pledges.amount)
-FROM projects
-INNER JOIN pledges
-ON projects.id = pledges.project_id
-GROUP BY projects.title;"
+    FROM projects
+    INNER JOIN pledges
+    ON projects.id = pledges.project_id
+    GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
 "SELECT users.name, users.age, SUM(pledges.amount)
-FROM users
-INNER JOIN pledges
-ON users.id = pledges.user_id
-GROUP BY users.id
-ORDER BY users.name ASC;"
+    FROM users
+    INNER JOIN pledges
+    ON users.id = pledges.user_id
+    GROUP BY users.id
+    ORDER BY users.name ASC;"
 end
 
 def selects_the_titles_and_amount_over_goal_of_all_projects_that_have_met_their_funding_goal
-"Write your SQL query Here"
+"SELECT projects.title,  SUM(pledges.amount) > projects.funding_goal AS over_goal
+FROM projects
+INNER JOIN pledges
+ON projects.id  = pledges.project_id
+GROUP BY  projects.id
+HAVING SUM(pledges.amount) > projects.funding_goal;"
 end
 
 def selects_user_names_and_amounts_of_all_pledges_grouped_by_name_then_orders_them_by_the_amount_and_users_name
