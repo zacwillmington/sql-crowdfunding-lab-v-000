fb2c7578dba77e6e7bafbabf5b8cc20727171048
Zac Willmington <zac.willmington@gmail.com>
2018-04-16 16:53:41 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -42,7 +42,11 @@ ORDER BY SUM(pledges.amount) ASC, users.name ASC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
-"Write your SQL query Here"
+"SELECT projects.category, pledges.amount
+FROM projects
+INNER JOIN pledges
+ON projects.id = pledges.project_id
+WHERE projects.category = 'music';"
 end
 
 def selects_the_category_name_and_the_sum_total_of_the_all_its_pledges_for_the_books_category
