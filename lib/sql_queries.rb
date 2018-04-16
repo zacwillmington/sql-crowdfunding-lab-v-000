54ea2b795270d1990d70fcf3a9850460f9681b07
Zac Willmington <zac.willmington@gmail.com>
2018-04-16 16:24:29 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -38,7 +38,7 @@ FROM users
 INNER JOIN pledges
 ON users.id = pledges.user_id
 GROUP BY users.name
-ORDER BY pledges.amount DESC;"
+ORDER BY users.name DESC;"
 end
 
 def selects_the_category_names_and_pledge_amounts_of_all_pledges_in_the_music_category
