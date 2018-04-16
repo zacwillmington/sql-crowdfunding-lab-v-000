38c8c364d581a6a08b1e8e3b73212bf98c2d0081
Zac Willmington <zac.willmington@gmail.com>
2018-04-16 15:37:30 +0000

    automatically backed up by learn

--- lib/sql_queries.rb
+++ lib/sql_queries.rb
@@ -10,7 +10,8 @@ def selects_the_titles_of_all_projects_and_their_pledge_amounts_alphabetized_by_
 "SELECT projects.title, pledges.amount
 FROM projects
 INNER JOIN pledges
-ON projects.id = pledges.project_id;"
+ON projects.id = pledges.project_id;
+GROUP BY projects.title;"
 end
 
 def selects_the_user_name_age_and_pledge_amount_for_all_pledges_alphabetized_by_name
